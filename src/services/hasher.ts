3834763c0b99f7a97e99ef50fc714439f5fa4556
Alejandro Arriola <alev_arriola@hotmail.com>
2025-09-12 09:47:33 -0300

    Delete src directory

--- src/services/hasher.ts
+++
@@ -1,6 +0,0 @@
-import { createHash } from "crypto";
-
-export function toBytes32Sha256(input: string | Buffer) {
-  const hex = createHash("sha256").update(input).digest("hex");
-  return { bytes32: "0x" + hex, hex };
-}
